6c3ff1a4328e6e346f3c8b4b61c9a67f6dec6810
baifan7574 <baifan7574@gmail.com>
2025-07-27 23:35:22 +0800

    One.

--- ads.js
+++ ads.js
@@ -1,17 +1,25 @@
 (function () {
-  // Anti-AdBlock Vignette
-  var ad1 = document.createElement("script");
-  ad1.src = "https://fpyf8.com/88/tag.min.js";
-  ad1.async = true;
-  ad1.setAttribute("data-zone", "9625896");
-  ad1.setAttribute("data-cfasync", "false");
-  document.head.appendChild(ad1);
+  // 广告 1：Anti-AdBlock Vignette
+  var vignetteAAD = document.createElement("script");
+  vignetteAAD.src = "https://fpyf8.com/88/tag.min.js";
+  vignetteAAD.async = true;
+  vignetteAAD.setAttribute("data-zone", "9625896");
+  vignetteAAD.setAttribute("data-cfasync", "false");
+  document.head.appendChild(vignetteAAD);
 
-  // Anti-AdBlock In-Page Push
-  var ad2 = document.createElement("script");
-  ad2.src = "https://fpyf8.com/88/tag.min.js";
-  ad2.async = true;
-  ad2.setAttribute("data-zone", "9625893");
-  ad2.setAttribute("data-cfasync", "false");
-  document.body.appendChild(ad2);
+  // 广告 2：Anti-AdBlock In-Page Push
+  var pushAAD = document.createElement("script");
+  pushAAD.src = "https://fpyf8.com/88/tag.min.js";
+  pushAAD.async = true;
+  pushAAD.setAttribute("data-zone", "9625893");
+  pushAAD.setAttribute("data-cfasync", "false");
+  document.body.appendChild(pushAAD);
+
+  // 广告 3：普通 Vignette（备用）
+  var vignetteNormal = document.createElement("script");
+  vignetteNormal.src = "https://fpyf8.com/88/tag.min.js";
+  vignetteNormal.async = true;
+  vignetteNormal.setAttribute("data-zone", "9625895");
+  vignetteNormal.setAttribute("data-cfasync", "false");
+  document.body.appendChild(vignetteNormal);
 })();
