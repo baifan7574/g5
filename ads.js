54d73135de5c611da73d35be6809bc4579cb2bc1
baifan7574 <baifan7574@gmail.com>
2025-07-27 23:19:52 +0800

    One.

--- ads.js
+++ ads.js
@@ -1,49 +1,17 @@
-// 普通 Vignette Banner
 (function () {
-  var v = document.createElement("script");
-  v.src = "https://fpyf8.com/88/tag.min.js";
-  v.async = true;
-  v.setAttribute("data-zone", "9622737");
-  v.setAttribute("data-cfasync", "false");
-  document.head.appendChild(v);
-})();
+  // Anti-AdBlock Vignette
+  var ad1 = document.createElement("script");
+  ad1.src = "https://fpyf8.com/88/tag.min.js";
+  ad1.async = true;
+  ad1.setAttribute("data-zone", "9625896");
+  ad1.setAttribute("data-cfasync", "false");
+  document.head.appendChild(ad1);
 
-// 普通 In-Page Push
-(function () {
-  var p = document.createElement("script");
-  p.src = "https://fpyf8.com/88/tag.min.js";
-  p.async = true;
-  p.setAttribute("data-zone", "9622735");
-  p.setAttribute("data-cfasync", "false");
-  document.body.appendChild(p);
-})();
-
-// Native Banner
-(function () {
-  var n = document.createElement("script");
-  n.src = "https://fpyf8.com/88/tag.min.js";
-  n.async = true;
-  n.setAttribute("data-zone", "9622736");
-  n.setAttribute("data-cfasync", "false");
-  document.body.appendChild(n);
-})();
-
-// Anti-AdBlock Vignette
-(function () {
-  var av = document.createElement("script");
-  av.src = "https://fpyf8.com/88/tag.min.js";
-  av.async = true;
-  av.setAttribute("data-zone", "9625895");
-  av.setAttribute("data-cfasync", "false");
-  document.head.appendChild(av);
-})();
-
-// Anti-AdBlock In-Page Push
-(function () {
-  var ap = document.createElement("script");
-  ap.src = "https://fpyf8.com/88/tag.min.js";
-  ap.async = true;
-  ap.setAttribute("data-zone", "9625893");
-  ap.setAttribute("data-cfasync", "false");
-  document.body.appendChild(ap);
+  // Anti-AdBlock In-Page Push
+  var ad2 = document.createElement("script");
+  ad2.src = "https://fpyf8.com/88/tag.min.js";
+  ad2.async = true;
+  ad2.setAttribute("data-zone", "9625893");
+  ad2.setAttribute("data-cfasync", "false");
+  document.body.appendChild(ad2);
 })();
